21b86dddca3cc61ae2857b81e37f399440702570
Mustofa <mustofa_sachi@hotmail.com>
2020-03-19 16:10:08 +0700

    add, delete, edit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import config from './config';
+
+if(firebase.apps.length === 0) 
+    firebase.initializeApp(config)
+
+export const firestore = firebase.firestore()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
